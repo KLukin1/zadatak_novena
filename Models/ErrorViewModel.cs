93c37929c22580622167c20e73431e5b66e0dc99
KLukin1 <klara.lukin@gmail.com>
2024-04-11 16:44:47 +0200

    initial layout

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Zadatak_Novena.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
