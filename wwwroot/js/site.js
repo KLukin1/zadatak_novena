8cedcf8cf6b0e98abd3ed3d5c9a68a17118eb487
KLukin1 <klara.lukin@gmail.com>
2024-04-14 15:05:47 +0200

    slider + responsive

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,13 +1,28 @@
 ﻿
 $(document).ready(function () {
-    var slider = $('.slider').bxSlider();
+    var slider = $('.slider').bxSlider({
+        onSlideBefore: updateSliderCaptions
+    });
+    updateSliderCaptions();
+
     $('.slider-back').click(function () {
         slider.goToPrevSlide();
+        updateSliderCaptions();
     });
 
     $('.slider-next').click(function () {
         slider.goToNextSlide();
+        updateSliderCaptions();
     });
 
+    function updateSliderCaptions() {
 
-})
+        var current = slider.getCurrentSlide();
+        var category = $('.slider .one-slide:not(.bx-clone)')[current].getAttribute("data-category");
+        var title = $('.slider .one-slide:not(.bx-clone)')[current].getAttribute("data-title");
+
+        $('.captions h3').text(category);
+        $('.captions h2').text(title);
+    }
+});
+
